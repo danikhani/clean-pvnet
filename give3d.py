4e06a94637f72a252726d74c3eb8c1efc3460e2d
Danial Hezarkhani <danial.hezarkhani@rwth-aachen.de>
2021-01-09 18:15:08 +0100

    example files added

--- give3d.py
+++ give3d.py
@@ -3,9 +3,21 @@ import io
 from plyfile import PlyData
 import numpy as np
 import yaml
+import math
 
 from lib.csrc.fps import fps_utils
 from lib.utils.linemod.opengl_renderer import OpenGLRenderer
+
+
+def calc_pts_diameter(pts):
+    diameter = -1
+    for pt_id in range(pts.shape[0]):
+        pt_dup = np.tile(np.array([pts[pt_id, :]]), [pts.shape[0] - pt_id, 1])
+        pts_diff = pt_dup - pts[pt_id:, :]
+        max_dist = math.sqrt((pts_diff * pts_diff).sum(axis=1).max())
+        if max_dist > diameter:
+            diameter = max_dist
+    return diameter
 
 
 def get_model_corners(model):
